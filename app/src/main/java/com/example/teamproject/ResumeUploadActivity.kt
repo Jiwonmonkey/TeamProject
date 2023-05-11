da93e932cddab86f4ff79400ac719eae212247ed
jiwon <jiwon0768@tukorea.ac.kr>
2023-05-11 15:46:32 +0900

    0511

--- app/src/main/java/com/example/teamproject/ResumeUploadActivity.kt
+++ app/src/main/java/com/example/teamproject/ResumeUploadActivity.kt
@@ -2,11 +2,17 @@ package com.example.teamproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class ResumeUploadActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resume_upload)
+
+        val toolbar: Toolbar = findViewById(R.id.resumeUploadToolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 }
